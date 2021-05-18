6f28a272d2782d5d00164caff5809dc2fef11284
vladislav <vladislav.051094@gmail.com>
2021-05-18 23:56:11 +0300

    added new social names

--- src/ninelines-sharing.js
+++ src/ninelines-sharing.js
@@ -48,32 +48,52 @@ export default class Share {
 	 * @returns {Window}
 	 */
 
-	static facebook(url, promise = null) {
-		return Share.openWindow(Share.getUrl('facebook', url), promise);
+	static fb(url, promise = null) {
+		return Share.openWindow(Share.getUrl('fb', url), promise);
 	}
 
 	static vk(url, promise = null) {
 		return Share.openWindow(Share.getUrl('vk', url), promise);
 	}
 
-	static twitter(url, promise = null) {
-		return Share.openWindow(Share.getUrl('twitter', url), promise);
+	static tw(url, promise = null) {
+		return Share.openWindow(Share.getUrl('tw', url), promise);
 	}
 
 	static ok(url, promise = null) {
 		return Share.openWindow(Share.getUrl('ok', url), promise);
 	}
 
+	static tg(url, promise = null) {
+		return Share.openWindow(Share.getUrl('tg', url), promise, undefined, null);
+	}
+
+	static facebook(url, promise = null) {
+		return Share.fb(url, promise);
+	}
+
+	static vkontakte(url, promise = null) {
+		return Share.vk(url, promise);
+	}
+
+	static twitter(url, promise = null) {
+		return Share.tw(url, promise);
+	}
+
+	static odnoklassniki(url, promise = null) {
+		return Share.ok(url, promise);
+	}
+
 	static telegram(url, promise = null) {
-		return Share.openWindow(Share.getUrl('telegram', url), promise);
+		return Share.tg(url, promise);
 	}
 
 }
 
 Share.urlList = {
-	facebook: 'https://facebook.com/sharer/sharer.php?u=',
+	fb: 'https://facebook.com/sharer/sharer.php?u=',
 	vk: 'https://vk.com/share.php?url=',
-	twitter: 'http://twitter.com/share?url=',
+	tw: 'http://twitter.com/share?url=',
 	ok: 'https://connect.ok.ru/offer?url=',
-	telegram: 'https://t.me/share/url?url=',
+	tg: 'https://t.me/share/url?url=',
 };
